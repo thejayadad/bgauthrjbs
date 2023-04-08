2ce4ef55181179e91ee3fd0926983e3bb5a32c4b
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-08 12:14:00 -0400

    update

--- front/src/components/Category.js
+++ front/src/components/Category.js
@@ -21,82 +21,55 @@ const Category = () => {
   </li>
 </ul>
         </div>
-    <div className='row'>
-      <div className='col-lg-4'>
-        <div className='item'>
+        <section id = "blogs" class = "py-5">
+        <div class = "container">
 
+            <div class = "row g-3">
+                <div class = "card border-0 col-md-6 col-lg-4 bg-transparent my-3">
+                    <img src = "https://images.pexels.com/photos/858466/pexels-photo-858466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt = "" />
+                    <div class = "card-body px-0">
+                        <h4 class = "card-title">Lorem ipsum, dolor sit amet consectetur adipisicing</h4>
+                        <p class = "card-text mt-3 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur repudiandae nostrum dolorem molestias odio. Sit fugit adipisci omnis quia itaque ratione iusto sapiente reiciendis, numquam officiis aliquid ipsam fuga.</p>
+                        <p class = "card-text">
+                            <small class = "text-muted">
+                                <span class = "fw-bold">Author: </span>John Doe
+                            </small>
+                        </p>
+                        <a href = "#" class = "btn">Read More</a>
+                    </div>
+                </div>
 
-        <img
-        className='img-fluid'
-        src="https://images.pexels.com/photos/2114365/pexels-photo-2114365.jpeg"
-        alt='catImage'
-        />
-        <h5>Title</h5>
-        <h6>Category</h6>
-      </div>
-    </div>
+                <div class = "card border-0 col-md-6 col-lg-4 bg-transparent my-3">
+                    <img src = "https://images.pexels.com/photos/858466/pexels-photo-858466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt = "" />
+                    <div class = "card-body px-0">
+                        <h4 class = "card-title">Lorem ipsum, dolor sit amet consectetur adipisicing</h4>
+                        <p class = "card-text mt-3 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur repudiandae nostrum dolorem molestias odio. Sit fugit adipisci omnis quia itaque ratione iusto sapiente reiciendis, numquam officiis aliquid ipsam fuga.</p>
+                        <p class = "card-text">
+                            <small class = "text-muted">
+                                <span class = "fw-bold">Author: </span>John Doe
+                            </small>
+                        </p>
+                        <a href = "#" class = "btn">Read More</a>
+                    </div>
+                </div>
 
+                <div class = "card border-0 col-md-6 col-lg-4 bg-transparent my-3">
+                    <img src = "https://images.pexels.com/photos/858466/pexels-photo-858466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt = "" />
+                    <div class = "card-body px-0">
+                        <h4 class = "card-title">Lorem ipsum, dolor sit amet consectetur adipisicing</h4>
+                        <p class = "card-text mt-3 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur repudiandae nostrum dolorem molestias odio. Sit fugit adipisci omnis quia itaque ratione iusto sapiente reiciendis, numquam officiis aliquid ipsam fuga.</p>
+                        <p class = "card-text">
+                            <small class = "text-muted">
+                                <span class = "fw-bold">Author: </span>John Doe
+                            </small>
+                        </p>
+                        <a href = "#" class = "btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    <div className='col-lg-4'>
-        <div className='item'>
-
-
-        <img
-        className='img-fluid'
-        src="https://images.pexels.com/photos/2114365/pexels-photo-2114365.jpeg"
-        alt='catImage'
-        />
-        <h5>Title</h5>
-        <h6>Category</h6>
-      </div>
-    </div>
-
-
-    <div className='col-lg-4'>
-        <div className='item'>
-
-
-        <img
-        className='img-fluid'
-        src="https://images.pexels.com/photos/2114365/pexels-photo-2114365.jpeg"
-        alt='catImage'
-        />
-        <h5>Title</h5>
-        <h6>Category</h6>
-      </div>
-    </div>
-
-    <div className='col-lg-4'>
-        <div className='item'>
-
-
-        <img
-        className='img-fluid'
-        src="https://images.pexels.com/photos/2114365/pexels-photo-2114365.jpeg"
-        alt='catImage'
-        />
-        <h5>Title</h5>
-        <h6>Category</h6>
-      </div>
-    </div>
-
-    <div className='col-lg-4'>
-        <div className='item'>
-
-
-        <img
-        className='img-fluid'
-        src="https://images.pexels.com/photos/2114365/pexels-photo-2114365.jpeg"
-        alt='catImage'
-        />
-        <h5>Title</h5>
-        <h6>Category</h6>
-      </div>
-    </div>
-
-
-
-    </div>
 
       </div>
 
--- front/src/components/Feature.js
+++ front/src/components/Feature.js
@@ -3,7 +3,48 @@ import React from 'react'
 const Feature = () => {
   return (
     <section className='mt-5'>
-      feature
+      <div class="background d-flex jusitfy-content-center align-items-center">
+        <div class="container">
+
+            <div class="features d-flex flex-column justify-content-center align-items-center gap-4 py-5 text-center">
+                <div class="features-header">
+                    <h2 class="h2 text-white">More than just a tool</h2>
+                    <small class="text-white-50">Explore what else we can do for you</small>
+                </div>
+
+                <div class="features-body d-grid text-white">
+                    <section class="feature-item d-flex flex-column gap-1 rounded-4 p-5 first-gradient">
+                        <i class="feature-icon rounded-pill bi-clipboard"></i>
+                        <strong class="feature-title">Project Management</strong>
+                        <small class="feature-subtitle text-white-50">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore.</small>
+                        <a href="#" class="text-white">Read More</a>
+                    </section>
+
+                    <section class="feature-item d-flex flex-column gap-1 rounded-4 p-5 second-gradient">
+                        <i class="feature-icon rounded-pill bi-clipboard"></i>
+                        <strong class="feature-title">Time Tracking</strong>
+                        <small class="feature-subtitle text-white-50">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore.</small>
+                        <a href="#" class="text-white">Read More</a>
+                    </section>
+
+                    <section class="feature-item d-flex flex-column gap-1 rounded-4 p-5 third-gradient">
+                        <i class="feature-icon rounded-pill bi-clipboard"></i>
+                        <strong class="feature-title">Resource Planing</strong>
+                        <small class="feature-subtitle text-white-50">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore.</small>
+                        <a href="#" class="text-white">Read More</a>
+                    </section>
+
+                    <section class="feature-item d-flex flex-column gap-1 rounded-4 p-5 fourth-gradient">
+                        <i class="feature-icon rounded-pill bi-clipboard"></i>
+                        <strong class="feature-title">Invoicing</strong>
+                        <small class="feature-subtitle text-white-50">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore.</small>
+                        <a href="#" class="text-white">Read More</a>
+                    </section>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </section>
   )
 }
