47bd64cab0a5aafea280bb5cfa6fed764d4e463b
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-08 12:19:47 -0400

    update

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Category from "../components/Category.js"
 import Feature from '../components/Feature'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Feature />
-      Home
-      <Category />
 
+      <Category />
+<Footer />
 
     </div>
   )
