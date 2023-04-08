3cc99b42cf066fc06ce64928ab21fdcd0f826c54
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-08 12:30:18 -0400

    update

--- front/src/components/Category.js
+++ front/src/components/Category.js
@@ -21,54 +21,21 @@ const Category = () => {
   </li>
 </ul>
         </div>
-        <section id = "blogs" class = "py-5">
-        <div class = "container">
+        <div className='container'>
+          <div className='col-md-4'>
+            <div className='single-blog'>
+            <p className='blog-meta'>Author <span>Date</span></p>
+            <img
 
-            <div class = "row g-3">
-                <div class = "card border-0 col-md-6 col-lg-4 bg-transparent my-3">
-                    <img src = "https://images.pexels.com/photos/858466/pexels-photo-858466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt = "" />
-                    <div class = "card-body px-0">
-                        <h4 class = "card-title">Lorem ipsum, dolor sit amet consectetur adipisicing</h4>
-                        <p class = "card-text mt-3 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur repudiandae nostrum dolorem molestias odio. Sit fugit adipisci omnis quia itaque ratione iusto sapiente reiciendis, numquam officiis aliquid ipsam fuga.</p>
-                        <p class = "card-text">
-                            <small class = "text-muted">
-                                <span class = "fw-bold">Author: </span>John Doe
-                            </small>
-                        </p>
-                        <a href = "#" class = "btn">Read More</a>
-                    </div>
-                </div>
+              scr="https://images.pexels.com/photos/1150988/pexels-photo-1150988.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt='blogImage'
+            />
 
-                <div class = "card border-0 col-md-6 col-lg-4 bg-transparent my-3">
-                    <img src = "https://images.pexels.com/photos/858466/pexels-photo-858466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt = "" />
-                    <div class = "card-body px-0">
-                        <h4 class = "card-title">Lorem ipsum, dolor sit amet consectetur adipisicing</h4>
-                        <p class = "card-text mt-3 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur repudiandae nostrum dolorem molestias odio. Sit fugit adipisci omnis quia itaque ratione iusto sapiente reiciendis, numquam officiis aliquid ipsam fuga.</p>
-                        <p class = "card-text">
-                            <small class = "text-muted">
-                                <span class = "fw-bold">Author: </span>John Doe
-                            </small>
-                        </p>
-                        <a href = "#" class = "btn">Read More</a>
-                    </div>
-                </div>
-
-                <div class = "card border-0 col-md-6 col-lg-4 bg-transparent my-3">
-                    <img src = "https://images.pexels.com/photos/858466/pexels-photo-858466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt = "" />
-                    <div class = "card-body px-0">
-                        <h4 class = "card-title">Lorem ipsum, dolor sit amet consectetur adipisicing</h4>
-                        <p class = "card-text mt-3 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur repudiandae nostrum dolorem molestias odio. Sit fugit adipisci omnis quia itaque ratione iusto sapiente reiciendis, numquam officiis aliquid ipsam fuga.</p>
-                        <p class = "card-text">
-                            <small class = "text-muted">
-                                <span class = "fw-bold">Author: </span>John Doe
-                            </small>
-                        </p>
-                        <a href = "#" class = "btn">Read More</a>
-                    </div>
-                </div>
             </div>
+          </div>
+
         </div>
-    </section>
+
 
 
       </div>
@@ -78,3 +45,54 @@ const Category = () => {
 }
 
 export default Category
+
+
+
+// <section id = "blogs" class = "py-5">
+//         <div class = "container">
+
+//             <div class = "row g-3">
+//                 <div class = "card border-0 col-md-6 col-lg-4 bg-transparent my-3">
+//                     <img src = "https://images.pexels.com/photos/858466/pexels-photo-858466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt = "" />
+//                     <div class = "card-body px-0">
+//                         <h4 class = "card-title">Lorem ipsum, dolor sit amet consectetur adipisicing</h4>
+//                         <p class = "card-text mt-3 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur repudiandae nostrum dolorem molestias odio. Sit fugit adipisci omnis quia itaque ratione iusto sapiente reiciendis, numquam officiis aliquid ipsam fuga.</p>
+//                         <p class = "card-text">
+//                             <small class = "text-muted">
+//                                 <span class = "fw-bold">Author: </span>John Doe
+//                             </small>
+//                         </p>
+//                         <a href = "#" class = "btn">Read More</a>
+//                     </div>
+//                 </div>
+
+//                 <div class = "card border-0 col-md-6 col-lg-4 bg-transparent my-3">
+//                     <img src = "https://images.pexels.com/photos/858466/pexels-photo-858466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt = "" />
+//                     <div class = "card-body px-0">
+//                         <h4 class = "card-title">Lorem ipsum, dolor sit amet consectetur adipisicing</h4>
+//                         <p class = "card-text mt-3 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur repudiandae nostrum dolorem molestias odio. Sit fugit adipisci omnis quia itaque ratione iusto sapiente reiciendis, numquam officiis aliquid ipsam fuga.</p>
+//                         <p class = "card-text">
+//                             <small class = "text-muted">
+//                                 <span class = "fw-bold">Author: </span>John Doe
+//                             </small>
+//                         </p>
+//                         <a href = "#" class = "btn">Read More</a>
+//                     </div>
+//                 </div>
+
+//                 <div class = "card border-0 col-md-6 col-lg-4 bg-transparent my-3">
+//                     <img src = "https://images.pexels.com/photos/858466/pexels-photo-858466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt = "" />
+//                     <div class = "card-body px-0">
+//                         <h4 class = "card-title">Lorem ipsum, dolor sit amet consectetur adipisicing</h4>
+//                         <p class = "card-text mt-3 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur repudiandae nostrum dolorem molestias odio. Sit fugit adipisci omnis quia itaque ratione iusto sapiente reiciendis, numquam officiis aliquid ipsam fuga.</p>
+//                         <p class = "card-text">
+//                             <small class = "text-muted">
+//                                 <span class = "fw-bold">Author: </span>John Doe
+//                             </small>
+//                         </p>
+//                         <a href = "#" class = "btn">Read More</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </section>
